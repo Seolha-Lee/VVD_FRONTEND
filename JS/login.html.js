310b5045516a9f8c8609f57de0daa66c6b785efc
설하 <seolha0@protonmail.ch>
2022-07-12 19:25:53 +0900

    🪓 :: %5, Unuseful code deletion

--- JS/login.html.js
+++ JS/login.html.js
@@ -12,26 +12,7 @@ loginfetch = () => {
             alert('아이디 또는 비밀번호가 맞지 않습니다.');
         } else if (response.message === 'SUCCESS') {
             this.props.history.push('/HTML/');
+            window.location.href = "/HTML/main.html";
         }
     });
 };
-
-$.ajax({
-    url:"/HTML/main.html",
-    type:'POST',
-    dataType:'json',
-    data:JSON.stringify(sendObject),
-    contentType: 'text/html;charset=UTF-8',
-    mimeType: 'application/json',
-    success:function(data) {
-        if(data.MESSAGE) {
-            alert("로그인성공");
-            window.location.href = "main.html";
-        } else {
-            alert("로그인실패");
-        }
-    },
-    error:function(data,status,er) {
-        alert("error: "+data+" status: "+status+" er:"+er);
-    }
-});
